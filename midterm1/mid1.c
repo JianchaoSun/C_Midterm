9f5ff177cd7704adec0d78b3234313f9479482e3
jianchaosun <clarksun97@gmail.com>
2019-03-12 18:46:03 -0700

    last commit

--- midterm1/mid1.c
+++ midterm1/mid1.c
@@ -17,12 +17,21 @@ int main(){
 	printf("    ");
 	for(int i=0;i<len;i++){
 		x[i] = i+1;
-		printf("%d     ",i+1);
+		if(i<9){
+		printf("%d     ",i+1);}
+		else if(i>=9&&i<100){
+			printf("%d    ",i+1);
+		}
 
 	}
+ printf("\n+");
+	for(int i=0;i<len*6;i++){
+         printf("%c",'-');
+		}
+
 	printf("\n");
 	int y[len][len];
-	printf("%d   ",1);
+	printf("%d|   ",1);
 
 	for(int i =0;i< len;i++){
 		for(int j=0;j<len;j++){
@@ -31,12 +40,19 @@ int main(){
 			printf("%d     ",y[i][j]);}
             else if(y[i][j]<100&&y[i][j]>=10){
             	printf("%d    ",y[i][j]);
-            }else {
-            	printf("%d    ",y[i][j]);
+            }else if(y[i][j]<1000&&y[i][j]>=100){
+            	printf("%d   ",y[i][j]);
+            }else{
+            	printf("%d  ",y[i][j]);
             }
 		}
 		if(i<=10){
-		printf("\n%d   ",i+2);}
+			if(i>7){
+				printf("\n%d|  ",i+2);
+			}
+			else{
+		printf("\n%d|   ",i+2);}
+		}
 	}
 
 }
